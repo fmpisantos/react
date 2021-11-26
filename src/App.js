eb1c6fbd9b781073b41c40c309e9df0c073b3048
fmpisantos <fmpi.santos@campus.fct.unl.pt>
2021-11-26 01:03:37 +0000

    Added about page to router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import '@styles/App.css';
 import Home from "@pages/Home"
+import About from "@pages/About"
 
 const App = props =>{
   return(
   <Router>
     <div className="nav-bar">
       <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
     </div>
     <Routes>
       <Route path="/" element={<Home {...props} name="Home"/>}/>
+      <Route path="/about" element={<About {...props} name="About"/>}/>
     </Routes>
   </Router>
   )
